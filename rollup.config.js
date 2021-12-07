ad7cfeac535bb08ae803ebbf9994a3984f179eb0
Fog3211 <fog3211@163.com>
2021-12-07 23:33:42 +0800

    chore: update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,52 @@
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
-import commonjs from '@rollup/plugin-commonjs';
-import {
-  terser
-} from 'rollup-plugin-terser';
-import alias from '@rollup/plugin-alias';
-import filesize from 'rollup-plugin-filesize';
-import path from 'path';
-import del from 'del';
-import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import alias from '@rollup/plugin-alias'
+import filesize from 'rollup-plugin-filesize'
+import path from 'path'
+import del from 'del'
+import pkg from './package.json'
 
 del.sync('dist/*')
 
-const pathResolve = p => path.resolve(__dirname, p);
+const pathResolve = (p) => path.resolve(__dirname, p)
 
 const now = new Date(
-  process.env.SOURCE_DATE_EPOCH ? process.env.SOURCE_DATE_EPOCH * 1000 : new Date().getTime()
-).toUTCString();
+  process.env.SOURCE_DATE_EPOCH ? process.env.SOURCE_DATE_EPOCH * 1000 : new Date().getTime(),
+).toUTCString()
 
 const banner = `/*
   @license
 	@airmus/logger v${pkg.version}
 	${now}
 	https://github.com/airmus/logger
-	Released under the MIT License By Fog3211.
-*/`;
+	Released under the MIT License By ${pkg.author}.
+*/`
 
 const treeshake = {
   moduleSideEffects: false,
   propertyReadSideEffects: false,
-  tryCatchDeoptimization: false
-};
+  tryCatchDeoptimization: false,
+}
 const nodePlugins = [
   alias({
     resolve: ['.js', '.json', '.ts'],
     entries: {
-      'src': pathResolve('@'),
-      '@utils': pathResolve('src/utils')
-    }
+      src: pathResolve('@')
+    },
   }),
   resolve({
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   }),
   json(),
   commonjs({
-    include: 'node_modules/**'
+    include: 'node_modules/**',
   }),
   typescript(),
-  filesize()
-];
+  filesize(),
+]
 
 export default () => {
   const commonJSBuild = {
@@ -63,9 +60,7 @@ export default () => {
       exports: 'auto',
       compact: true,
     },
-    plugins: [
-      ...nodePlugins
-    ],
+    plugins: [...nodePlugins],
     treeshake,
     strictDeprecations: true,
   }
@@ -76,31 +71,33 @@ export default () => {
       dir: 'dist/es',
       format: 'es',
       sourcemap: false,
-      minifyInternalExports: false
-    }
+      minifyInternalExports: false,
+    },
   }
   const browserBuilds = {
     ...commonJSBuild,
     plugins: [
       ...nodePlugins,
       resolve({
-        browser: true
+        browser: true,
       }),
       terser({
         module: true,
         output: {
-          comments: 'some'
-        }
-      })
+          comments: 'some',
+        },
+      }),
     ],
-    output: [{
-      file: 'dist/logger.browser.js',
-      format: 'umd',
-      name: '$logger',
-      banner,
-      compact: true
-    }]
+    output: [
+      {
+        file: 'dist/logger.browser.js',
+        format: 'umd',
+        name: '$logger',
+        banner,
+        compact: true,
+      },
+    ],
   }
 
-  return [commonJSBuild, esmBuild, browserBuilds];
+  return [commonJSBuild, esmBuild, browserBuilds]
 }
